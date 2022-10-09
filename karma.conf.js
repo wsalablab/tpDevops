f952eb96bcd147c643a17c71f5d0cecace31097e
wsalablab <49472773+wsalablab@users.noreply.github.com>
2022-10-09 14:53:42 +0200

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -13,17 +13,17 @@ module.exports = function (config) {
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      jasmine: {
+      //jasmine: { //mod1
         // you can add configuration options for Jasmine here
         // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
         // for example, you can disable the random execution with `random: false`
         // or set a specific seed with `seed: 4321`
-      },
+      //}, //mod1
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
-    },
+    //jasmineHtmlReporter: { //mod1
+    //  suppressAll: true // removes the duplicated traces //mod1
+    //}, //mod1
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage/my-app'),
       subdir: '.',
@@ -39,6 +39,11 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    customLaunchers: {
+      ChromeHeadlessPuppeteer: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu']
+      }
   });
 };
