f1dd3c312e984f5160e197b3b7c7dbd39fea27fe
wsalablab <49472773+wsalablab@users.noreply.github.com>
2022-10-12 13:32:46 +0200

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istambul-reporter'),
+      'karma-coverage-istambul-reporter',
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
